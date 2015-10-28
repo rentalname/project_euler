3ec2726bc45c568b84b30d7f097fdc17e95c9ef2
rentalname <agenda21st@gmail.com>
2015-10-28 16:39:22 +0900

    [solve] Problem 10 Summation of primes

---
+++ 010.rb
@@ -0,0 +1,13 @@
+<<NOTE.!
+  Problem 10 Summation of primes
+
+  The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+  Find the sum of all the primes below two million.
+NOTE
+
+require 'prime'
+
+x = Prime.lazy.take_while{|n| n < 2_000_000 }.inject(:+)
+
+puts "A. #{x}"

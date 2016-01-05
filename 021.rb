847ab1f2c600b92f39afa0f268fa6647e8d414d8
rentalname <agenda21st@gmail.com>
2016-01-05 19:01:33 +0900

    [solve] Problem 21 Amicable numbers

---
+++ 021.rb
@@ -0,0 +1,47 @@
+<<~NOTE
+  Problem 21 Amicable numbers
+
+    Let d(n) be defined as the sum of proper divisors of n (numbers less than n which divide evenly into n).
+    If d(a) = b and d(b) = a, where a ≠ b, then a and b are an amicable pair and each of a and b are called amicable numbers.
+
+    For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore d(220) = 284. The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
+
+    Evaluate the sum of all the amicable numbers under 10000.
+NOTE
+require 'set'
+
+class Fixnum
+  require 'prime'
+
+  def factors
+    set = Set[1]
+    current = self
+    Prime.each(self - 1) do |prime|
+      div, mod = current.divmod(prime)
+
+      if mod == 0
+        set = set + set.map{|s| s * prime}
+        if div == 1
+          return set.to_a[0..-2]
+        else
+          current = div
+          redo
+        end
+      else
+        next
+      end
+    end || set
+  end
+end
+
+amicas = Set.new
+1.upto(10000) do |num|
+  p1 = num.factors.inject(:+)
+  p2 = p1.factors.inject(:+)
+
+  amicas.merge [num, p1] if num == p2 && num != p1
+end
+
+ans = amicas.inject(:+)
+
+puts "A. #{ans}"

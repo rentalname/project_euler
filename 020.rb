eba6aa2531b3a938f943af8adf5dfe84d791c98f
rentalname <agenda21st@gmail.com>
2016-01-05 18:07:04 +0900

    [solve] Problem 20 Factorial digit sum

---
+++ 020.rb
@@ -0,0 +1,13 @@
+<<NOTE.!
+  Problem 20 Factorial digit sum
+
+    n! means n × (n − 1) × ... × 3 × 2 × 1
+
+    For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+    and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+    Find the sum of the digits in the number 100!
+NOTE
+
+answer = [*2..100].inject(:*).to_s.each_char.map(&:to_i).inject(:+)
+puts "A. #{answer}"

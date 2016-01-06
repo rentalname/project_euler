1a99cc73d3d4f1f0bfd101ae398ae698f36c3117
rentalname <agenda21st@gmail.com>
2016-01-06 19:41:02 +0900

    [solve] Problem 24 Lexicographic permutations

---
+++ 024.rb
@@ -0,0 +1,16 @@
+<<~NOTE
+  Problem 24 Lexicographic permutations
+
+    A permutation is an ordered arrangement of objects.
+    For example, 3124 is one possible permutation of the digits 1, 2, 3 and 4.
+    If all of the permutations are listed numerically or alphabetically, we call it lexicographic order.
+    The lexicographic permutations of 0, 1 and 2
+
+    are:
+      012   021   102   120   201   210
+
+    What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
+NOTE
+answer = [*0..9].permutation.to_a[999999].map(&:to_s).join
+
+puts "A. #{answer}"
